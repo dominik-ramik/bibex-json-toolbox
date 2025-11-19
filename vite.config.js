1660233b039ca9a1a3836752eaaf1dc6504d7039
dominik-ramik <26235743+dominik-ramik@users.noreply.github.com>
2025-11-19 21:29:17 +1100

    chore: use terser for minification

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    minify: true,
+    minify: "terser",
+    terserOptions: {
+      format: {
+        comments: false,
+        beautify: false,
+      },
+      compress: true,
+      mangle: true,
+    },
     lib: {
       entry: 'src/main.js',
       name: 'bibexJsonToolbox',
